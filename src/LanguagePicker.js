a284cb95d2d614937db7a68722cac57f29317d4e
Kenny Ngo <kenny.ngo.design@gmail.com>
2021-01-18 13:21:57 -0800

    simulated a click on the first icon for setLanguage

---
+++ src/LanguagePicker.js
@@ -0,0 +1,27 @@
+import PropTypes from "prop-types";
+import React from "react";
+
+function LanguagePicker({ setLanguage }) {
+  const languages = [
+    { code: "en", symbol: "🇺🇸" },
+    { code: "emoji", symbol: "😊" },
+  ];
+
+  const languageIcons = languages.map((lang) => (
+    <span
+      data-test="language-icon"
+      key={lang.code}
+      onClick={() => setLanguage(lang.code)}
+    >
+      {lang.symbol}
+    </span>
+  ));
+
+  return <div data-test="component-language-picker">{languageIcons} </div>;
+}
+
+LanguagePicker.propTypes = {
+  setLanguage: PropTypes.func.isRequired,
+};
+
+export default LanguagePicker;
